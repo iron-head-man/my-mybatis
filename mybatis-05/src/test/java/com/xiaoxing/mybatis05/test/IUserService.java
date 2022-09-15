52550a5d7b31e89c581d938ec2f994002a466f8c
邢恒 <xxingheng@163.com>
2022-09-15 23:58:58 +0800

    [05-Mapper XML的解析和注册使用]:第5章：数据源的解析、创建和使用,完成测试

---
+++ mybatis-05/src/test/java/com/xiaoxing/mybatis05/test/IUserService.java
@@ -0,0 +1,23 @@
+package com.xiaoxing.mybatis05.test;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author xxingheng@hand-china.com 2022/09/15 11:10
+ */
+public interface IUserService {
+
+    void add(String s);
+
+    /**
+     * 增加一个用户
+     */
+    public void addUser();
+
+    /**
+     * 编辑账户
+     */
+    public void editUser();
+}
---
+++ mybatis-05/src/test/java/com/xiaoxing/mybatis05/test/ProxyTest.java
@@ -0,0 +1,36 @@
+package com.xiaoxing.mybatis05.test;
+
+import org.junit.Test;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author xxingheng@hand-china.com 2022/09/15 15:52
+ */
+public class ProxyTest {
+
+
+    // @Test
+    // public void test_11(){
+    // IUserService userService = new UserServiceImpl();
+    // MyProxy myProxy = new MyProxy(userService);
+    // IUserService proxy =(IUserService) myProxy.getProxy();
+    // proxy.add("1");
+    // }
+//    public static void main(String[] args) throws Throwable {
+//
+//    }
+
+    @Test
+    public void test_0001() throws Throwable {
+        IUserService userService = new UserServiceImpl();
+        MyProxy handler = new MyProxy(userService);
+
+        // 根据目标生成代理对象
+        IUserService proxy = (IUserService) handler.getProxy();
+        proxy.addUser();
+        proxy.add("1");
+    }
+}
